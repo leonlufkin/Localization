5e75a6c2d1d20e0308018351156588371b13fa0c
leonlufkin <72353550+leonlufkin@users.noreply.github.com>
2024-08-01 18:26:39 -0400

    again

--- localization/utils/__init__.py
+++ localization/utils/__init__.py
@@ -3,7 +3,7 @@ from localization.utils.admin import make_key, make_ica_key
 # from localization.utils.launcher import get_timestamp, tupify, get_executor#, submit_jobs, product_kwargs
 from localization.utils.measurement import ipr, entropy, position_mean_var, entropy_sort, mean_sort, var_sort
 from localization.utils.modeling import build_gaussian_covariance, build_sine_covariance, build_gaussian_covariance_numpy, build_non_gaussian_covariance, build_ising_covariance, build_pre_gaussian_covariance, gabor_real, gabor_imag, build_DRT, iterate_kron, normal_adjust, uniform_adjust, no_adjust
-from counting.utils.submit import submit_jobs, product_kwargs
+from localization.utils.submit import submit_jobs, product_kwargs
 from localization.utils.visualization import plot_receptive_fields, plot_rf_evolution
 
 __all__ = (
