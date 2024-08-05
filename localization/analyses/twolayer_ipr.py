6e2cd1426de2e48f21d66d54e9d77e5fb516dc1c
leonlufkin <72353550+leonlufkin@users.noreply.github.com>
2024-08-05 00:58:27 -0400

    Update twolayer_ipr.py

--- localization/analyses/twolayer_ipr.py
+++ localization/analyses/twolayer_ipr.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     config.update(dict(
         num_epochs=10000, evaluation_interval=100,
         num_hiddens=10,
+        learning_rate=1.,
         batch_size=5000,
     ))
     # 
