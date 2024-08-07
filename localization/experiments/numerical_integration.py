5ebb565265d429f60e33e220391007144cf0b490
leonlufkin <72353550+leonlufkin@users.noreply.github.com>
2024-08-06 20:04:26 -0400

    Update numerical_integration.py

--- localization/experiments/numerical_integration.py
+++ localization/experiments/numerical_integration.py
@@ -92,9 +92,9 @@ if __name__ == '__main__':
         model_cls=models.SimpleNet,
         use_bias=False,
         optimizer_fn=optax.sgd,
-        learning_rate=0.05,#0.01,
-        batch_size=25000,#50000,#10000,
-        num_epochs=20000,#10000,#5000,
+        learning_rate=0.2,#0.05,#0.01,
+        batch_size=100000,#50000,#10000,
+        num_epochs=5000,#10000,#5000,
         dataset_cls=datasets.NonlinearGPDataset,
         xi=(0.3, 0.7), #(0.7, 0.3,),
         # num_steps=10000,
@@ -104,7 +104,7 @@ if __name__ == '__main__':
         init_fn=models.xavier_normal_init,
         loss_fn='mse',
         save_=True,
-        evaluation_interval=200,#100,#50,
+        evaluation_interval=50,#100,#50,
     )
     w_model = simulate_or_load(**c)[0][:,0]
     mini_key = f'seed={c["seed"]}_L={c["num_dimensions"]}_g={c["gain"]}_is={c["init_scale"]}_lr={c["learning_rate"]}_b={c["batch_size"]}_xi={c["xi"][0]},{c["xi"][1]}_T={c["num_epochs"]}'
